3543c8fc62f6af4825e09eb5c1be4778ad98fd90
Omar <omarbaldidk@gmail.com>
2020-09-30 13:17:15 +0200

    Applied recursion to web scraping

--- scraping.js
+++ scraping.js
@@ -1,24 +1,63 @@
 const puppeteer = require('puppeteer');
 
-async function startScraping(URL) {
+const maxTime = 2;
+let time;
+let currentLinksToSearch;
+let isFirstTime = true;
+let nodeDataArray;
+let linkDataArray;
+let index = 0;
+let browser;
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(URL, { waitUntil: 'networkidle0', timeout: 0 });
+async function startScraping(URLS) {
 
-    const linksPage = await page.evaluate(() => {
-        return Array
-        .from(document.querySelectorAll('.mw-body-content p .mw-redirect'))
-        .slice(0, 5)
-        .map(elem => ({
-            text: elem.innerText,
-            href: elem.href
-        }));
-    });
+    if (isFirstTime) {
+        time = 0;
+        index = 1;
+        isFirstTime = false;
+        linkDataArray = new Array();
+        nodeDataArray = new Array({ key: index, text: URLS[0].text });
+        browser = await puppeteer.launch();
+    };
 
-    await page.close();
+    currentLinksToSearch = new Array();
+
+    for (let URL of URLS) {
+        const page = await browser.newPage();
+        await page.goto(URL.href, { waitUntil: 'networkidle0', timeout: 0 });
+
+        const linksPage = await page.evaluate(() => {
+            return Array
+            .from(document.querySelectorAll('.mw-body-content p .mw-redirect'))
+            .slice(0, 5)
+            .map(elem => ({
+                text: elem.innerText,
+                href: elem.href
+            }));
+        });
+
+ 
+        currentLinksToSearch = currentLinksToSearch.concat(linksPage);
+
+        linksPage.forEach(link => {
+            index += 1;
+            nodeDataArray.push({ key: index, text: link.text });
+            const parent = nodeDataArray.find(x => x.text === URL.text);
+            linkDataArray.push({ from: parent.key, to: index });
+        });
+
+        await page.close();
+    }
+
+    time += 1;
+
+    if (time >= maxTime) {
+        isFirstTime = true;
+        return { nodeDataArray, linkDataArray }
+    } else {
+        return startScraping(currentLinksToSearch);
+    }
     
-    return linksPage;
 }
 
 module.exports = { startScraping };
--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.use(express.json());
 //Routes
 app.get('/api/links', async(req, res) => {
 
-    const URL = 'https://en.wikipedia.org/wiki/Dog';
+    const baseURL = 'https://en.wikipedia.org/wiki';
+    const startPoint = [{ text: 'Dog', href: `${baseURL}/Dog` }];
 
     try {
-        const response = await startScraping(URL);
-        console.log(response);
-        res.status(200).json(response);
+        const { nodeDataArray, linkDataArray } = await startScraping(startPoint);
+        res.status(200).json({ nodeDataArray, linkDataArray });
     } catch(err) {
         res.status(500).json({ message: err.message });
     }
